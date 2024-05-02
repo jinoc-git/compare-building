196593f80e57b8c3a7247fb5b4c904cff420d0e1
jinoc <choiae369@gmail.com>
2024-05-02 20:05:11 +0900

    Design: main 태그 css 변경

--- src/components/common/layouts/RootLayout.tsx
+++ src/components/common/layouts/RootLayout.tsx
@@ -5,11 +5,7 @@ interface Props {
 }
 
 const RootLayout = ({ children }: Props) => {
-  return (
-    <>
-      <main className="w-full border border-black m-3 p-1">{children}</main>
-    </>
-  );
+  return <main className="w-full min-h-screen pt-14 px-3">{children}</main>;
 };
 
 export default RootLayout;
