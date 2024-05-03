252ffcef32da3e19f0d562e28b35546e90accb3d
jinoc <choiae369@gmail.com>
2024-05-04 04:51:35 +0900

    Feat: fetchBuildingChartById api 함수 추가

--- src/api/building.ts
+++ src/api/building.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 import type { BuildingDetailType, BuildingType } from 'types/building.type';
+import type { ChartType } from 'types/chart.type';
 
 export const fetchBuildings = async () => {
   const res = await axios.get<{ data: BuildingType[] }>('/buildings');
@@ -10,6 +11,12 @@ export const fetchBuildings = async () => {
 
 export const fetchBuildingDetailById = async (id: string) => {
   const res = await axios.get<{ data: BuildingDetailType }>(`/building/${id}`);
+
+  return res.data.data;
+};
+
+export const fetchBuildingChartById = async (id: string) => {
+  const res = await axios.get<{ data: ChartType }>(`/chart/${id}`);
 
   return res.data.data;
 };
